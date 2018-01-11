f00a35b7e329a8a53edb6a55834da9c3347a2ed6
Neo <Neo@Neo-PC.cht.com.tw>
2018-01-11 11:27:24 +0800

    restful test2

--- src/main/java/com/cht/test/web/JAXActivator.java
+++
@@ -1,9 +0,0 @@
-package com.cht.test.web;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("rest")
-public class JAXActivator extends Application {
-    // Left empty intentionally
-}
